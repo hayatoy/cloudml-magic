fa45e70ccdd8c12d32a1e5971ba7c72e31c7433d
hayatoy <hayatoyoshikawa@Hayatos-MacBook-Pro.local>
2017-03-29 23:10:31 +0900

    add magic python codes

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='cloudmlmagic',
+      version='0.0.1',
+      description='Jupyter Notebook Magics for Google Cloud ML Engine',
+      author='Hayato Yoshikawa',
+      url='https://github.com/hayatoy/cloudml-magic',
+      packages=find_packages(),
+      install_requires=['google-api-python-client'],
+      license="MIT",
+      )
